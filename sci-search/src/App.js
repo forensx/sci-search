6f7d3cda0e2e7ed5a6a31d7170c1fabb75940066
Aniket Pant <aniketpant@jefcoed.com>
2020-03-07 16:01:06 -0600

    Added layout with sider

--- sci-search/src/App.js
+++ sci-search/src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from "react";
-import "./App.css";
 import "antd/dist/antd.css";
-import { Layout, Menu } from "antd";
-
-const { Header, Content, Footer, Sider } = Layout;
+import { Layout, Menu } from 'antd';
+const { Header, Content, Sider } = Layout;
 
 export default class App extends Component {
+  state = {
+    collapsed: false
+  };
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
   render() {
     return (
       <div className="App">
         <Layout>
-          <Header>SciSearch</Header>
-          <Layout>
-            <Content>main content</Content>
-            <Sider>
+          <Header style={{ color: "white" }}>Sci Search</Header>
+          <Layout style={{ minHeight: "100vh" }}>
+            <Sider
+              collapsible
+              collapsed={this.state.collapsed}
+              onCollapse={this.onCollapse}
+            >
               <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
                 <Menu.Item key="1">
                   <span>Case 1</span>
                 </Menu.Item>
+                <Menu.Item key="2">
+                  <span>Case 2</span>
+                </Menu.Item>
               </Menu>
             </Sider>
+            <Content>
+              <div>Search here</div>
+              <div>Papers here</div>
+            </Content>
           </Layout>
         </Layout>
       </div>
