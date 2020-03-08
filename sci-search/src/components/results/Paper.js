9faf059fb97eb79c94a7a20c7d6eeb5eb1d1bc60
Aniket Pant <aniketpant@jefcoed.com>
2020-03-08 11:34:18 -0500

    Added styling and fixed some data rendering

--- sci-search/src/components/results/Paper.js
+++ sci-search/src/components/results/Paper.js
@@ -5,6 +5,19 @@ import { Row, Col } from "antd";
 const { Paragraph } = Typography;
 
 export class Paper extends Component {
+
+  journalCapitalizationCase = journalName => {
+    var splitStr = journalName.toLowerCase().split(" ");
+    for (var i = 0; i < splitStr.length; i++) {
+      // You do not need to check if i is larger than splitStr length, as your for does that for you
+      // Assign it back to the array
+      splitStr[i] =
+        splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    // Directly return the joined string
+    return splitStr.join(" ");
+  }
+
   render() {
     let abstractRender;
 
@@ -40,7 +53,7 @@ export class Paper extends Component {
     if (this.props.results.journal) {
       journalRender = (
         <Descriptions.Item label="Journal">
-          {this.props.results.journal}
+          {this.journalCapitalizationCase(this.props.results.journal)}
         </Descriptions.Item>
       );
     } else {
@@ -63,6 +76,54 @@ export class Paper extends Component {
       keywordRender = (
         <Descriptions.Item label="Keywords">
           Keywords not found
+        </Descriptions.Item>
+      );
+    }
+
+    let geneListRender;
+
+    if (this.props.results.genes) {
+      geneListRender = (
+        <Descriptions.Item label="Genes">
+          {this.props.results.genes.join(", ")}
+        </Descriptions.Item>
+      );
+    } else {
+      geneListRender = (
+        <Descriptions.Item label="Genes">Genes not found</Descriptions.Item>
+      );
+    }
+
+    let dateRender;
+
+    if (this.props.results.UTCDatetime === null) {
+      console.log("UTC null");
+      if (this.props.results.pubDate.year === null) {
+        dateRender = (
+          <Descriptions.Item label="Date">Date not complete</Descriptions.Item>
+        );
+      } else if (this.props.results.pubDate.month === null) {
+        dateRender = (
+          <Descriptions.Item label="Date">Date not complete</Descriptions.Item>
+        );
+      } else if (this.props.results.pubDate.day === null) {
+        dateRender = (
+          <Descriptions.Item label="Date">Date not complete</Descriptions.Item>
+        );
+      }
+    } else {
+      console.log(
+        "Date is: ",
+        this.props.results.pubDate.day,
+        this.props.results.pubDate.month,
+        this.props.results.pubDate.year
+      );
+      const date = new Date(this.props.results.UTCDatetime);
+      console.log(date);
+
+      dateRender = (
+        <Descriptions.Item label="Date">
+          {date.toLocaleDateString()}
         </Descriptions.Item>
       );
     }
@@ -98,16 +159,14 @@ export class Paper extends Component {
         >
           <Descriptions size="default">{abstractRender}</Descriptions>
           <Descriptions column={3}>
-            <Descriptions.Item label="Date">
-              {this.props.results.pubDate.month}-
-              {this.props.results.pubDate.day}-{this.props.results.pubDate.year}
-            </Descriptions.Item>
+            {dateRender}
             <Descriptions.Item label="Database">
               {this.props.results.database}
             </Descriptions.Item>
             {journalRender}
           </Descriptions>
-          <Descriptions column={1}>{keywordRender}</Descriptions>
+          <Descriptions column={2}>{keywordRender}</Descriptions>
+          <Descriptions column={2}>{geneListRender}</Descriptions>
         </PageHeader>
       </div>
     );
--- sci_search_api.py
+++ sci_search_api.py
@@ -7,6 +7,9 @@ from medrxiv_api import medrxiv
 import math
 import json
 from rake_nltk import Rake
+import datetime
+import re
+import numpy as np
 
 # Uses stopwords for english from NLTK, and all puntuation characters.
 r = Rake(min_length=2, max_length=6)
@@ -20,28 +23,43 @@ class e(Resource):
         search_params = search_param
         page_num = numResults
         pubmed_result = pubmed(search_params, page_num)
-        #biorxiv_result = bioarchive(search_params, page_num)
+        # biorxiv_result = bioarchive(search_params, page_num)
         scholar_result = google(search_params, math.ceil(page_num/10))
-        #medrxiv_result = medrxiv(search_params, math.ceil(page_num/10))
+        # medrxiv_result = medrxiv(search_params, math.ceil(page_num/10))
 
         pubmed_arr = pubmed_result['results']
-        #biorxiv_arr = biorxiv_result['results']
+        # biorxiv_arr = biorxiv_result['results']
         scholar_arr = scholar_result['results']
-        #medrxiv_arr = medrxiv_result['results']
-        #combined = pubmed_arr + biorxiv_arr + scholar_arr + medrxiv_arr
+        # medrxiv_arr = medrxiv_result['results']
+        # combined = pubmed_arr + biorxiv_arr + scholar_arr + medrxiv_arr
         combined = pubmed_arr + scholar_arr
 
-        '''for i in range(len(combined)):
-            text = combined[i]['abstract']
-            # r.extract_keywords_from_text(text)
-            # To get keyword phrases ranked highest to lowest.
-            keywords_extracted = ['Test1', 'Test2']
-            #keywords_extracted = r.get_ranked_phrases()[0:4]
+        for i in range(len(combined)):
+            if combined[i]['abstract']:
+                text = combined[i]['abstract']
+                r.extract_keywords_from_text(text)
+                # To get keyword phrases ranked highest to lowest.
+                keywords_extracted = r.get_ranked_phrases()[0:4]
 
-            combined[i]['keywords'] = keywords_extracted
+                combined[i]['keywords'] = keywords_extracted
+                genes = (re.findall("[A-Z]+[-]?[0-9]?(?![a-z!@#$%^&*(.?\":{}|<>])", text))
+                genes = [x for x in genes if len(x) > 2 and len(x) < 7 and x != "DNA" and x != "RNA"]
+                counts = genes.count
+                genes_unique = sorted(np.unique(genes), key=counts)[::-1]
+                combined[i]['genes'] = genes_unique[0:4]
+            else:
+                combined[i]['keywords'] = ["No Abstract Found"]
+                combined[i]['genes'] = ["No Genes Found"]
 
-            # print(combined[i]['pubDate'])
-        '''
+            date = combined[i]['pubDate']
+            try:
+                date_str = "{} {} {}".format(
+                    date['month'], date['day'], date['year'])
+                utcDatetime = datetime.datetime.strptime(date_str, '%b %d %Y')
+                combined[i]['UTCDatetime'] = utcDatetime
+            except:
+                combined[i]['UTCDatetime'] = None
+
         # return results of search here
         return jsonify({'results': combined})
 
