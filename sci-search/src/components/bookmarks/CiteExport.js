af29aa587690178d4798d0014b1ee5f7addf69ec
LonnieWebb <32077775+LonnieWebb@users.noreply.github.com>
2020-04-06 19:48:07 -0500

    Added export of static data

--- sci-search/src/components/bookmarks/CiteExport.js
+++ sci-search/src/components/bookmarks/CiteExport.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
 import { Button } from "antd";
 import { ExportOutlined } from "@ant-design/icons";
+import { useDispatch } from "react-redux";
+import Cite from 'citation-js';
+import { save } from 'save-file'
 
-export class CiteExport extends Component {
-  render() {
+function ExportCitations(exports) {
+  let citations = new Cite(exports)
+
+  let output = citations.format('bibliography', {
+    style: 'csl',
+    template: "apa"
+  })
+  console.log("cite output", output)
+
+  return(save(output, 'example.txt'))
+}
+
+function CiteExport() {
+
+
     return (
       <div>
-        <Button icon={<ExportOutlined />} style={{marginLeft:"2.5%", width:"95%"}}>Export Citations</Button>
+        <Button icon={<ExportOutlined />} style={{marginLeft:"2.5%", width:"95%"}} onClick={exports => ExportCitations(["10.1186/s12885-019-5765-3","10.1038/s41586-020-1969-6"])}>Export Citations</Button>
       </div>
     );
   }
-}
+
 
 export default CiteExport;
