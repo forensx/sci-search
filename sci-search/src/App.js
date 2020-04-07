2815b2c41f51d0e763f1da552cfc87161c673489
Aniket Pant <aniketpant@jefcoed.com>
2020-04-07 14:12:20 -0500

    Added content pane

--- sci-search/src/App.js
+++ sci-search/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { Layout, Menu, Breadcrumb } from "antd";
+import { Layout, Menu } from "antd";
+import Search from "./components/Search";
+import ResultList from "./components/results/ResultList";
 import {
   UserOutlined,
   LaptopOutlined,
@@ -15,7 +17,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Layout style = {{height: "100vh"}}>
+        <Layout style={{ height: "100vh" }}>
           <Header className="header">
             <div className="logo" />
             <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
@@ -77,20 +79,17 @@ export default class App extends Component {
               </Menu>
             </Sider>
             <Layout style={{ padding: "0 24px 24px" }}>
-              <Breadcrumb style={{ margin: "16px 0" }}>
-                <Breadcrumb.Item>Home</Breadcrumb.Item>
-                <Breadcrumb.Item>List</Breadcrumb.Item>
-                <Breadcrumb.Item>App</Breadcrumb.Item>
-              </Breadcrumb>
               <Content
                 className="site-layout-background"
                 style={{
                   padding: 24,
                   margin: 0,
                   minHeight: 280,
+                  marginTop: 24,
                 }}
               >
-                Content
+                <Search />
+                <ResultList />
               </Content>
             </Layout>
           </Layout>
