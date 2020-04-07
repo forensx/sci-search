0ff6c632ab2c7c1e27ba24668713b6366946a196
LonnieWebb <32077775+LonnieWebb@users.noreply.github.com>
2020-04-06 22:53:13 -0500

    APA Export Added

--- sci-search/src/components/bookmarks/CiteExport.js
+++ sci-search/src/components/bookmarks/CiteExport.js
@@ -4,10 +4,17 @@ import { ExportOutlined } from "@ant-design/icons";
 import { useDispatch } from "react-redux";
 import Cite from 'citation-js';
 import { save } from 'save-file'
+import { useSelector } from "react-redux";
 
-function ExportCitations(exports) {
-  let citations = new Cite(exports)
+function ExportCitations(exports, allCases) {
 
+  let curCase = "wads"
+  let myCase = allCases[curCase].bookmarks
+  let DOIs = []
+  myCase.forEach(x => DOIs.push(x.doi))
+  console.log("all cases are", allCases)
+  console.log("all DOIs are", DOIs)
+  let citations = new Cite(DOIs)
   let output = citations.format('bibliography', {
     style: 'csl',
     template: "apa"
@@ -18,11 +25,11 @@ function ExportCitations(exports) {
 }
 
 function CiteExport() {
-
+const allCases = useSelector((state) => state.bookmarksByCase);
 
     return (
       <div>
-        <Button icon={<ExportOutlined />} style={{marginLeft:"2.5%", width:"95%"}} onClick={exports => ExportCitations(["10.1186/s12885-019-5765-3","10.1038/s41586-020-1969-6"])}>Export Citations</Button>
+        <Button icon={<ExportOutlined />} style={{marginLeft:"2.5%", width:"95%"}} onClick={exports => ExportCitations(["10.1186/s12885-019-5765-3","10.1038/s41586-020-1969-6"], allCases)}>Export Citations</Button>
       </div>
     );
   }
